7f238b64b5ed88fb263d1d20258aff26ff88f1bd
Katyha04-k <katya.degtyareva28041997@gmail.com>
2025-06-25 16:02:33 +0300

    Добавила тест "Добавление нового питомца"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,7 +1,9 @@
 from http.client import responses
 
 import allure
+import jsonschema
 import requests
+from .schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
@@ -44,3 +46,40 @@ class TestPet:
 
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
+
+    @allure.feature("Добавление нового питомца")
+    def test_add_pet(self):
+        with allure.step("Подготовка данных для создания питомца"):
+            payload = {
+                "id": 10,
+                "name": "doggie",
+                "category": {
+                    "id": 1,
+                    "name": "Dogs"
+                },
+                "photoUrls": [
+                    "string"
+                ],
+                "tags": [
+                    {
+                        "id": 0,
+                        "name": "string"
+                    }
+                ],
+                "status": "available"
+            }
+
+        with allure.step("Отправка запроса на создание питомца"):
+            response = requests.post(url=f"{BASE_URL}/pet", json=payload)
+
+        with allure.step("Проверка статуса ответа и валидация JSON-схемы"):
+                assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+                jsonschema.validate(response.json(),PET_SCHEMA)
+
+        with allure.step("Проверка параметров питомца в ответе"):
+            assert response.json()['id'] == payload['id'], "id питомца не совпадает с ожидаемым"
+            assert response.json()['name'] == payload['name'], "name питомца не совпадает с ожидаемым"
+            assert response.json()['status'] == payload['status'], "status питомца не совпадает с ожидаемым"
+            assert response.json()['category'] == payload['category'], "категория питомца не совпадает с ожидаемым"
+            assert response.json()['photoUrls'] == payload['photoUrls'], "photoUrls питомца не совпадает с ожидаемым"
+            assert response.json()['tags'] == payload['tags'], "tags питомца не совпадает с ожидаемым"
